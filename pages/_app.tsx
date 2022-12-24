55e0057077abda0164f3b0b97541310842d849ac
Oein <oein0219@gmail.com>
2022-12-24 23:59:50 +0900

    Redirect to /notKR when not KR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,29 @@ import "../styles/alert.css";
 import type { AppProps } from "next/app";
 
 // use
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // alert component
 import Alert from "../components/Alert";
 import Jo from "../components/eventAlert/jocoding";
 
+import axios from "axios";
+import { useRouter } from "next/router";
+
 export default function App({ Component, pageProps }: AppProps) {
   let [alertOpen, setAlertOpen] = useState(true);
+
+  let router = useRouter();
+
+  useEffect(() => {
+    axios.get("http://ip-api.com/json").then((v) => {
+      console.log(v.data);
+      if (v.data.countryCode != "KR") {
+        router.push("/notKR");
+        return;
+      }
+    });
+  }, [router]);
 
   return (
     <>
