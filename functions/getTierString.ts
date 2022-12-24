e6674e9ad6589486c0626c18cc42746590790351
Oein <oein0219@gmail.com>
2022-12-25 00:04:55 +0900

    file name is what does it do

--- functions/getTierString.ts
+++ functions/getTierString.ts
@@ -1,9 +1,13 @@
+import style from "../styles/pop.module.css";
+
 const tierArr = ["diamond", "platinum", "gold", "silver", "bronze"];
 const scrArr = [300e4, 75e4, 25e4, 5e4, 1e4];
 export default function tier(score: string) {
   let int_score = parseInt(score);
   for (let i = 0; i < scrArr.length; i++)
-    if (int_score >= scrArr[i]) return tierArr[i];
+    if (int_score >= scrArr[i]) {
+      return style[tierArr[i]];
+    }
 
   return "none";
 }
