3ba499a29afe2ee8c1fa23e7b16087f1aa99144d
Oein <oein0219@gmail.com>
2022-12-24 09:03:17 +0900

    change macro statement

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,13 @@ export default function Main() {
   let router = useRouter();
   if (typeof window !== "undefined" && window) {
     if (
-      (typeof window.navigator !== "undefined" && window.navigator) ||
-      window.localStorage.getItem("macro")
-    ) {
-      if (window.navigator.webdriver) router.push("/usingMacro");
-    }
+      typeof window.navigator !== "undefined" &&
+      window.navigator &&
+      window.navigator.webdriver
+    )
+      router.push("/usingMacro");
+
+    if (window.localStorage.getItem("macro")) router.push("/usingMacro");
     if (
       window.localStorage.getItem("schoolName") == null ||
       window.localStorage.getItem("schoolCode") == null
