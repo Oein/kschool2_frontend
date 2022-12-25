bf9d81e635ae14d38cde6ac634fe1899988ba7df
Oein <oein0219@gmail.com>
2022-12-25 22:35:10 +0900

    add log

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,32 @@ import Jo from "../components/eventAlert/jocoding";
 export default function App({ Component, pageProps }: AppProps) {
   let [alertOpen, setAlertOpen] = useState(true);
 
+  console.log(`
+  ********************************************************************
+   
+   K   KK             SSSSS     CCCC   H   H   OOOOO    OOOOO   L
+   K KK              SS        CC  CC  H   H  OO   OO  OO   OO  L
+   KK       -----     SSSSS    C       HHHHH  O     O  O     O  L
+   K KK                   SS   CC  CC  H   H  OO   OO  OO   OO  L
+   K   KK             SSSSS     CCCC   H   H   OOOOO    OOOOO   LLLLL
+
+             CCCCC    L      IIIII   CCCCC   K   KK
+            CC   CC   L        I    CC   CC  K KK
+            C         L        I    C        KK
+            CC   CC   L        I    CC   CC  K KK
+             CCCCC    LLLLL  IIIII   CCCCC   K   KK            v2.0.0
+
+  ********************************************************************
+
+     Developers      (@Github)            Special Thanks to
+      - AwesomeDream (@awesomeSwam)        - JoCoding (https://www.youtube.com/@jocoding)
+      - SungHyun     (@Oein)               - 
+      - sdnight5     (@sdnight5)           - 
+      - sangho129    (@sangho129)          - 
+
+  ********************************************************************
+            `);
+
   return (
     <>
       {alertOpen ? (
