84be015c8c0d5ad8b64d427892659016c6125e58
Oein <oein0219@gmail.com>
2022-12-26 22:51:56 +0900

    add school skin

--- functions/getSeason.ts
+++ functions/getSeason.ts
@@ -7,6 +7,8 @@ export default function getSession() {
   if ((mon == 1 && day <= 2) || (mon == 12 && day == 31)) return "-newyear";
   if (mon == 5 && day == 5) return "-child";
   if (mon == 10 && day == 9) return "-han";
+  if (mon == 2 && day >= 14 && day <= 16) return "-sc";
+  if (mon == 3 && day >= 1 && day <= 3) return "-sc";
 
   return "";
 }
