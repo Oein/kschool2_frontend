29b85dd7fd97176f58f64c81bbf655bf8e3026ae
Oein <oein0219@gmail.com>
2022-12-26 20:13:26 +0900

    ads

--- pages/pop.tsx
+++ pages/pop.tsx
@@ -281,6 +281,11 @@ export default function Pop() {
             className={classNames([style.popImage])}
             style={getPopImage(popCount % 2)}
           ></div>
+          <iframe
+            width="728"
+            height="90"
+            src="https://tab2.clickmon.co.kr/pop/wp_ad_728.php?PopAd=CM_M_1003067%7C%5E%7CCM_A_1124013%7C%5E%7CAdver_M_1046207&mon_rf=REFERRER_URL&mon_direct_url=URLENCODE_PASSBACK_INPUT"
+          ></iframe>
           <div
             className={style.leaderboardContx}
             onClick={() => {
