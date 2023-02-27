1e9e6acf1b9729f9550172ddfefb5bb56b2102fe
Oein <oein0219@gmail.com>
2023-02-27 19:51:29 +0900

    devmode without console

--- utils/log.ts
+++ utils/log.ts
@@ -2,9 +2,13 @@ import { toast } from "react-toastify";
 
 if (
   typeof window != "undefined" &&
-  typeof (window as any).devmode == "undefined"
+  typeof (window as any).devmode == "undefined" &&
+  typeof location != "undefined"
 )
-  (window as any).devmode = 0;
+  if (location.href.includes("?devmode="))
+    if (location.href.includes("?devmode=2")) (window as any).devmode = 2;
+    else if (location.href.includes("?devmode=1")) (window as any).devmode = 1;
+    else (window as any).devmode = 0;
 
 export default function log(...messages: any[]) {
   if (!(window as any).devmode) return;
