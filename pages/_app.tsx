2e3e44f9261a3417f93ce4136a75b52cf1880dcb
Oein <oein0219@gmail.com>
2023-04-01 14:08:06 +0900

    good bye kschool.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -68,44 +68,16 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       {alertOpen ? (
-        <Alert
-          title="이벤트 알림"
-          onClose={() => {
-            setAlertOpen(false);
-          }}
-        >
-          학교 간식차 이벤트 실제로 진행하겠습니다.
-          <br />
-          협의가 성공한 학교에 전교생을 워한 간식차를 보내드리겠습니다.
-          <br />
-          <br />
-          간식차비는{" "}
-          <a className="link fontSize" href="https://www.youtube.com/@jocoding">
-            조코딩
-          </a>
-          님께서 지원해주신다고 하셨습니다.
-          <br />
-          루머를 현실로 만들어주신{" "}
-          <a className="link fontSize" href="https://www.youtube.com/@jocoding">
-            조코딩
-          </a>
-          님께 진심으로 감사드립니다.
-          <br />
-          자세한 내용은 밑에 있는 링크들을 확인해주세요.
-          <br />
-          <a className="link fontSize" href="https://youtu.be/yBW1HdjRGww">
-            유튜브 영상 바로가기
-          </a>
-          <br />
-          <a
-            className="link fontSize"
-            href="https://twitter.com/_awesome_dream/status/1605161640931909632"
-          >
-            트위터 공지 바로가기
-          </a>
-          <br />
-          <br />
-          개발자 일동 : 서버 버그 고쳤습니다!
+        <Alert title="이벤트 알림" onClose={() => {}}>
+          <img
+            src="/1.png"
+            style={{
+              width: "min(600px, 70vw)",
+              position: "relative",
+              left: "50%",
+              transform: "translateX(-50%)",
+            }}
+          />
         </Alert>
       ) : null}
 
